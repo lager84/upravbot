41e5e9218623661b7c79f77bbf9d841e2f7c5bbb
Константин Козлов <kozlov@live.ru>
2025-04-30 20:59:01 +0300

    After Marge

--- src/components/add-edit-account/AddEditAccount.tsx
+++ src/components/add-edit-account/AddEditAccount.tsx
@@ -3,180 +3,180 @@ import { useFormik } from "formik";
 import { TState } from "../../utils/typesTS";
 import InputComponent from "../../components/imput-component/InputComponent";
 import { useMutation } from "@apollo/client";
-import { UPDATE_ACCOUNT , ADD_ACCOUNT , DELETE_ACCOUNT} from "../../apollo/QLAccount";
+import {
+  UPDATE_ACCOUNT,
+  ADD_ACCOUNT,
+  DELETE_ACCOUNT,
+} from "../../apollo/QLAccount";
 import Loader from "../loader/Loader";
 import { useNavigate } from "react-router-dom";
 import { Checkbox, CheckboxChangeEvent } from "primereact/checkbox";
 import accountStore from "../../services/accountsStore";
 import imgBin from "../../img/ic-bin.svg";
-import { ConfirmDialog } from 'primereact/confirmdialog';
-import { Toast } from 'primereact/toast';
-import * as Yup from 'yup';
+import { ConfirmDialog } from "primereact/confirmdialog";
+import { Toast } from "primereact/toast";
+import * as Yup from "yup";
 
 type TPropsState = {
-    card:boolean
-    
-}
-type TProps = TPropsState & TState
+  card: boolean;
+};
+type TProps = TPropsState & TState;
 
 interface Category {
-  name: string; 
+  name: string;
   key: string;
 }
 
-
-
-
-const AddEditAccount: FC<TProps> = ({card , Email , FirstName , SecondName , userID , phone_number , role , GivenName , password  }) => {
-
+const AddEditAccount: FC<TProps> = ({
+  card,
+  Email,
+  FirstName,
+  SecondName,
+  userID,
+  phone_number,
+  role,
+  GivenName,
+  password,
+}) => {
   const toast = useRef<Toast>(null);
   const [visible, setVisible] = useState<boolean>(false);
-      
-      const accept =  () => {
-      
-        const event = new Event('click');
-        handleDelete();
-       
-    }
-    const reject = () => {
-      toast.current?.show({ severity: 'warn', summary: 'Отмена', detail: 'Вы отменили удаление пользователя', life: 3000 });
-  
-  }
 
-
-
-
-
-  const handleDelete = () => {
-   
-    deleteAccount({
-      variables: {
-        userID:userID
-      }
- 
+  const accept = () => {
+    const event = new Event("click");
+    handleDelete();
+  };
+  const reject = () => {
+    toast.current?.show({
+      severity: "warn",
+      summary: "Отмена",
+      detail: "Вы отменили удаление пользователя",
+      life: 3000,
     });
   };
 
-var userInfo = accountStore((state) => state);
+  const handleDelete = () => {
+    deleteAccount({
+      variables: {
+        userID: userID,
+      },
+    });
+  };
+
+  var userInfo = accountStore((state) => state);
 
   const categories: Category[] = [
-    { name: 'Менеджер', key: '50C5D24A-D585-473C-82B2-411DA4120FA5' },
-    { name: 'Ответственный', key: 'B6FAFCA4-67E0-4CC6-ACE7-F285DEF6A0B6' },
-    { name: 'Исполнитель', key: 'CD1FEC0D-7828-4C6A-8BE3-25EF9E1A12C3' }
-    
-];
+    { name: "Менеджер", key: "50C5D24A-D585-473C-82B2-411DA4120FA5" },
+    { name: "Ответственный", key: "B6FAFCA4-67E0-4CC6-ACE7-F285DEF6A0B6" },
+    { name: "Исполнитель", key: "CD1FEC0D-7828-4C6A-8BE3-25EF9E1A12C3" },
+  ];
 
-// const category = categories.find(cat => role?.includes(cat.name));
-// const categoryKey = category ? category.key : null;
+  // const category = categories.find(cat => role?.includes(cat.name));
+  // const categoryKey = category ? category.key : null;
 
-const categoryIndex = categories.filter(cat => role?.includes(cat.name));
+  const categoryIndex = categories.filter((cat) => role?.includes(cat.name));
 
+  const [selectedCategories, setSelectedCategories] = useState<Category[]>(
+    card ? categoryIndex : []
+  );
 
+  const onCategoryChange = (e: CheckboxChangeEvent) => {
+    let _selectedCategories = [...selectedCategories];
 
-const [selectedCategories, setSelectedCategories] = useState<Category[]>(card ? categoryIndex : []);
+    if (e.checked) _selectedCategories.push(e.value);
+    else
+      _selectedCategories = _selectedCategories.filter(
+        (category) => category.key !== e.value.key
+      );
 
-const onCategoryChange = (e: CheckboxChangeEvent) => {
-  let _selectedCategories = [...selectedCategories];
+    setSelectedCategories(_selectedCategories);
+  };
 
-  if (e.checked)
-      _selectedCategories.push(e.value);
-  else
-      _selectedCategories = _selectedCategories.filter(category => category.key !== e.value.key);
+  const navigate = useNavigate();
+  const [showWarning, setShowWarning] = useState(false);
+  const [showWarningIdentity, setShowWarningIdentity] = useState(false);
+  const [showWarningIdentityUpdate, setShowWarningIdentityUpdate] =
+    useState(false);
 
-  setSelectedCategories(_selectedCategories);
-  
- 
-
-};
-
-
-    const navigate = useNavigate();
-    const [showWarning, setShowWarning] = useState(false);
-    const [showWarningIdentity, setShowWarningIdentity] = useState(false);
-    const [showWarningIdentityUpdate, setShowWarningIdentityUpdate] = useState(false);
-
-    
-    const formik = useFormik<TState>({
+  const formik = useFormik<TState>({
     initialValues: {
-      Email:card ? Email : "",
-      FirstName:card ? FirstName :"",
-      GivenName:card ? GivenName :"",
-      SecondName:card? SecondName :"",
-      phone_number:card? phone_number :"",
-      password:card? "123456789" :"",
-      role:card ? role : [],
-      userID:card? userID :"",
-     
-
-    }
-    , onSubmit: (values) => {
-      
+      Email: card ? Email : "",
+      FirstName: card ? FirstName : "",
+      GivenName: card ? GivenName : "",
+      SecondName: card ? SecondName : "",
+      phone_number: card ? phone_number : "",
+      password: card ? "123456789" : "",
+      role: card ? role : [],
+      userID: card ? userID : "",
+    },
+    onSubmit: (values) => {
       if (selectedCategories.length === 0) {
         setShowWarning(true);
         return;
-    }
-      
-      if(card){
-    updateAccount({
-        variables: {
-          email: values.Email,
-          firstName: values.FirstName,
-          middleName: values.GivenName,
-          password: values.password,
-          phoneNumber: values.phone_number,
-          secondName: values.SecondName,
-          userID: values.userID,
-          role:selectedCategories.map((item) =>(item.name)) //
-        }
-      }, );
+      }
 
-        }
-        else{
-      addAccount({
-        variables: {
-          clientID:userInfo.userID,
-          email: values.Email,
-          firstName: values.FirstName,
-          middleName: values.GivenName,
-          secondName: values.SecondName,
-          password: values.password,
-          phoneNumber: values.phone_number,
-          userName: values.Email,
-          role:selectedCategories.map((item) =>(item.name)) //
-        }
-      }, );
-  
-            }
+      if (card) {
+        updateAccount({
+          variables: {
+            email: values.Email,
+            firstName: values.FirstName,
+            middleName: values.GivenName,
+            password: values.password,
+            phoneNumber: values.phone_number,
+            secondName: values.SecondName,
+            userID: values.userID,
+            role: selectedCategories.map((item) => item.name), //
+          },
+        });
+      } else {
+        addAccount({
+          variables: {
+            clientID: userInfo.userID,
+            email: values.Email,
+            firstName: values.FirstName,
+            middleName: values.GivenName,
+            secondName: values.SecondName,
+            password: values.password,
+            phoneNumber: values.phone_number,
+            userName: values.Email,
+            role: selectedCategories.map((item) => item.name), //
+          },
+        });
+      }
     },
   });
 
-
-
-  const [updateAccount, {data:data_update , loading:load_update, error:error_update }] = useMutation(UPDATE_ACCOUNT, {
+  const [
+    updateAccount,
+    { data: data_update, loading: load_update, error: error_update },
+  ] = useMutation(UPDATE_ACCOUNT, {
     onCompleted: () => {
       if (data_update?.updateAccount.succeeded) {
         navigate(-1);
       } else {
         setShowWarningIdentityUpdate(true);
       }
-    },});
+    },
+  });
 
-    const [addAccount, {data:data_add , loading:load_add, error:error_add }] = useMutation(ADD_ACCOUNT, {
+  const [addAccount, { data: data_add, loading: load_add, error: error_add }] =
+    useMutation(ADD_ACCOUNT, {
       onCompleted: () => {
         if (data_add?.addUser.succeeded) {
           navigate(-1);
         } else {
           setShowWarningIdentity(true);
         }
-      },});
+      },
+    });
 
-      
-     
-
-      const [deleteAccount, { loading:load_del, error:error_del }] = useMutation(DELETE_ACCOUNT, {
-        onCompleted: () => {
-          navigate(-1);
-        },});
+  const [deleteAccount, { loading: load_del, error: error_del }] = useMutation(
+    DELETE_ACCOUNT,
+    {
+      onCompleted: () => {
+        navigate(-1);
+      },
+    }
+  );
 
   const GenPas = () => {
     var bigCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -198,7 +198,7 @@ const onCategoryChange = (e: CheckboxChangeEvent) => {
   if (load_del) return <Loader />;
   if (error_del) return <div>${error_del.message}</div>;
 
-console.log(data_update);
+  console.log(data_update);
 
   return (
     <div className="col-sm-12 p-0">
@@ -209,24 +209,47 @@ console.log(data_update);
             onSubmit={formik.handleSubmit}
           >
             <div className="flexHoriz w-100">
-           {card?   
-           <>
-           <h2 className="font24b textBlack mb-3">Изменить данные пользователя</h2>
-              <Toast ref={toast} position="bottom-right" />
-              <ConfirmDialog className="modal-contentProject  bgWhite rounded16 p-4 shadow   col-12 col-lg-6" acceptLabel="Удалить" acceptClassName="btn btn1 h56 mr-2" rejectLabel="Отмена" rejectClassName="btn btn1 h56 mr-2ml-auto btn btn1 h56 outline shadow-none flexCenter CancelProject" group="declarative"  visible={visible} onHide={() => setVisible(false)} message="Вы уверены что хотите удалить проект?" 
-                  icon="pi pi-exclamation-triangle" accept={accept} reject={reject} />
-                <button id="DeleteUO" type="button" onClick={() => setVisible(true)} className="transp border-0 ml-auto">
-                  <img
-                    src={imgBin}
-                    className="mr-3 position-absolute d-flex ml-n4 "
-                    alt=""
-                  ></img>
-                  <span id="delspan" className="font16b reddish">
-                    Удалить пользователя
-                  </span>
-                </button>
+              {card ? (
+                <>
+                  <h2 className="font24b textBlack mb-3">
+                    Изменить данные пользователя
+                  </h2>
+                  <Toast ref={toast} position="bottom-right" />
+                  <ConfirmDialog
+                    className="modal-contentProject  bgWhite rounded16 p-4 shadow   col-12 col-lg-6"
+                    acceptLabel="Удалить"
+                    acceptClassName="btn btn1 h56 mr-2"
+                    rejectLabel="Отмена"
+                    rejectClassName="btn btn1 h56 mr-2ml-auto btn btn1 h56 outline shadow-none flexCenter CancelProject"
+                    group="declarative"
+                    visible={visible}
+                    onHide={() => setVisible(false)}
+                    message="Вы уверены что хотите удалить проект?"
+                    icon="pi pi-exclamation-triangle"
+                    accept={accept}
+                    reject={reject}
+                  />
+                  <button
+                    id="DeleteUO"
+                    type="button"
+                    onClick={() => setVisible(true)}
+                    className="transp border-0 ml-auto"
+                  >
+                    <img
+                      src={imgBin}
+                      className="mr-3 position-absolute d-flex ml-n4 "
+                      alt=""
+                    ></img>
+                    <span id="delspan" className="font16b reddish">
+                      Удалить пользователя
+                    </span>
+                  </button>
                 </>
-           :<h2 className="font24b textBlack mb-3">Добавить пользователя</h2>}
+              ) : (
+                <h2 className="font24b textBlack mb-3">
+                  Добавить пользователя
+                </h2>
+              )}
             </div>
 
             <div className="flexHoriz justify-content-between mt-3">
@@ -309,26 +332,52 @@ console.log(data_update);
               required={true}
             />
 
-<h5 className="font16b pt-2 pb-1">Роли</h5>
+            <h5 className="font16b pt-2 pb-1">Роли</h5>
 
-<div className="card flex justify-content-center">
-            <div className="flex flex-column gap-3">
+            <div className="card flex justify-content-center">
+              <div className="flex flex-column gap-3">
                 {categories.map((category) => {
-                    return (
-                        <div key={category.key} className="flex align-items-center">
-                            <Checkbox   name="category"   inputId={category.key} value={category} onChange={onCategoryChange } checked={selectedCategories.some((item) => item.key === category.key)  } />
-                            <label htmlFor={category.key} className="ml-2">
-                                {category.name}
-                            </label>                           
-                        </div>                     
-                    );
-                })}   
-                {showWarning && <div style={{color:"red"}}>Пожалуйста, выберите хотя бы одну роль</div>}             
+                  return (
+                    <div key={category.key} className="flex align-items-center">
+                      <Checkbox
+                        name="category"
+                        inputId={category.key}
+                        value={category}
+                        onChange={onCategoryChange}
+                        checked={selectedCategories.some(
+                          (item) => item.key === category.key
+                        )}
+                      />
+                      <label htmlFor={category.key} className="ml-2">
+                        {category.name}
+                      </label>
+                    </div>
+                  );
+                })}
+                {showWarning && (
+                  <div style={{ color: "red" }}>
+                    Пожалуйста, выберите хотя бы одну роль
+                  </div>
+                )}
+              </div>
             </div>
-        </div>
 
-        {showWarningIdentity && data_add.addUser?.errors?.map((errors:any) =>{ return( <div key={errors?.code} style={{color:"red"}}>{errors?.description}</div>)})}
-        {showWarningIdentityUpdate && data_update.updateAccount?.errors?.map((errors:any) =>{ return( <div key={errors?.code} style={{color:"red"}}>{errors?.description}</div>)})}   
+            {showWarningIdentity &&
+              data_add.addUser?.errors?.map((errors: any) => {
+                return (
+                  <div key={errors?.code} style={{ color: "red" }}>
+                    {errors?.description}
+                  </div>
+                );
+              })}
+            {showWarningIdentityUpdate &&
+              data_update.updateAccount?.errors?.map((errors: any) => {
+                return (
+                  <div key={errors?.code} style={{ color: "red" }}>
+                    {errors?.description}
+                  </div>
+                );
+              })}
 
             <div className="row mt-3 mb-3">
               <div className="col-sm-12">
